6508460bea0ddf77037ad9100455e57b08c47f61
Yale Alves <yalealves1999@gmail.com>
2023-05-31 19:30:56 -0300

    Input to create new task and style of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+
+import TodoList from './components/TodoList'
+import { IoMdAddCircle } from 'react-icons/io';
+import { ToDoContext } from './context/ToDoContext';
+
+import "./style/App.css";
 
 const App: React.FC = () => {
+
+  const [inputText, setInputText] = useState('');
+  const { handleAddTask, errorMessage } = useContext(ToDoContext);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  function handleNewTask() {
+    handleAddTask(inputText);
+    setInputText('');
+  }
+
   return (
-    <div>Hello World!</div>
+    <div className="app-container">
+      <h1 className="app-title">Todo List</h1>
+      <div className="input-container">
+        <input
+          className="todo-input"
+          type="text"
+          placeholder="Add a new todo"
+          value={inputText}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={handleNewTask}>
+          <IoMdAddCircle className="add-icon" />
+        </button>
+      </div>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <TodoList />
+    </div>
   )
 }
 

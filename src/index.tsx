016e4e39aa07ab4c56e9cea2de27b22546a7b7db
Yale Alves <yalealves1999@gmail.com>
2023-05-31 19:33:48 -0300

    Provider Added in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ToDoProvider } from './context/ToDoContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ToDoProvider>
+      <App />
+    </ToDoProvider>
   </React.StrictMode>
 );
